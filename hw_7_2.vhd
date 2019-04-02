9e5b3b38d0c186646df9c97aa8fcf0e93e4df9d2
has58 <has58@njit.edu>
2019-04-02 13:31:39 -0400

    time second part is included

--- hw_7_2.vhd
+++ hw_7_2.vhd
@@ -1,3 +1,9 @@
+-- Author: Haider Ali Siddiquee
+-- date : 4/2/2019
+-- time 12:15
+-- Hw7.vhd
+-- To calculate 1. Most frequent input character in every 100ns
+--              2. highest occurred input character with respect to time
 Library IEEE;
 Use IEEE.std_logic_1164.All;
 
@@ -6,6 +12,7 @@ Entity smoother is
         c_out : out std_logic);
   end Entity;
 
+-- Architecture to calculate number of time input character come in every 100ns
 Architecture smoother_arch of smoother is
 	-- array with index data type std_logic
 	-- std_index_array ('U', 'X','0','1','Z','W','L','H','-')
@@ -15,6 +22,7 @@ Architecture smoother_arch of smoother is
 	-- internal array to store frequency of the charecter
 	signal main_array : std_index_array := reset_array;
 	-- funtion to find the most occured charecter
+	--constant first_time : time := 0ns;
 	function max (signal array_max  : in std_index_array)
     		return std_logic is
     		variable biggest: std_logic := '0';
@@ -55,3 +63,57 @@ Architecture smoother_arch of smoother is
 			end if;
 		end process;
 	end smoother_arch;
+
+-- Architecture to calculate longest time occurred input character come in every 100ns
+Architecture smoother_arch_time of smoother is
+
+	-- array with index data type std_logic
+	-- std_index_array ('U', 'X','0','1','Z','W','L','H','-')
+	type std_index_array is array(std_logic) of time;
+	-- this is a constant to resent the array after 100ns
+	constant reset_array: std_index_array :=(0ns,0ns,0ns,0ns,0ns,0ns,0ns,0ns,0ns);
+	-- internal array to store frequency of the charecter
+	signal main_array : std_index_array := reset_array;
+	-- funtion to find the longest occured charecter
+	function max (signal array_max  : in std_index_array)
+    		return std_logic is
+    		variable biggest: std_logic := '0';
+    		begin
+			for i in std_index_array'range  loop
+          			if ((array_max(biggest) < array_max(i)) or (array_max(biggest) = array_max(i))) then
+            				biggest :=i;
+          			end if;
+        		end loop;
+        		return biggest;
+     		end max;
+	-- procedure to update the array
+	procedure counter(signal main_array : inout std_index_array;
+				variable last_time: inout time;
+                        			signal input : in std_logic) is
+					begin
+						main_array(input'last_value) <= main_array(input'last_value) + (now-last_time);
+						last_time := now;
+					end counter;
+	-- internal clock
+	signal clk : bit :='0';
+	begin
+		clk_p2: process
+				begin
+				clk <= NOT(clk);
+				wait for 100ns;
+				end process clk_p2;
+		main_p2: process(clk, c_in)
+			-- to trach time for the last character
+			variable last_time : time :=0ns;
+				begin
+					if (clk'event) then
+						--give the output
+						c_out <= max(main_array);
+					end if;
+					if (c_in'event) then
+						--calculate the time
+						--update the array
+						counter (main_array, last_time,c_in);
+					end if;
+				end process main_p2;
+	end smoother_arch_time;
